3f414c229bdaec2d6f6445095245ae810bdae3f6
griflet <dipendra497@gmail.com>
2022-05-09 00:51:30 +0530

    init

--- client.c
+++ client.c
@@ -1,22 +1,20 @@
 /*
-Command writing format:
 
-filename server_inpaddress portno
+filename server_ipaddress portno
+
 argv[0] filename
 argv[1] server_ipaddress
 argv[2] portno
 
 */
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <unistd.h> // use for read write and close
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h> 
-// Defines the hostent structure - used to store info about a given host eg., host name and ipv4 address.
+#include <netdb.h> /* use for hostent*/
 
 void error(const char *msg)
 {
@@ -28,6 +26,10 @@ int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
+    /*The hostent structure is used by functions to store
+     information about a given host, such as host name,
+      IPv4 address,
+    */
     struct hostent *server;
 
     char buffer[256];
@@ -37,12 +39,12 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    portno = atoi(argv[2]); // Convert string to number.
+    portno = atoi(argv[2]); 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
 
-    server = gethostbyname(argv[1]); // Inbuilt function that gets the host using the ip address of the server.
+    server = gethostbyname(argv[1]); 
     if (server ==NULL)
     {
         fprintf(stderr, "Error! No such host.");
@@ -50,27 +52,34 @@ int main(int argc, char *argv[])
 
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length); // Transfer info from *server to serv_addr. // Need to read more later.
-    serv_addr.sin_port = htons(portno); // Host to network short
+    // bcopy will help us to copy from *server to serv_addr
+    bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length); 
+    // takes a 16-bit number in host byte order and returns a 16-bit number in network byte order used in TCP/IP networks
+    serv_addr.sin_port = htons(portno); 
+
+    // connect
     if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
         error("Connection Failed!");
 
     while(1) 
     {
-        bzero(buffer, 255); // Clear the buffer
+        // clear
+        bzero(buffer, 255); 
+        // required to pass string to server
         fgets(buffer, 255, stdin);
+        // write to server
         n = write(sockfd, buffer, strlen(buffer));
         if (n < 0)
             error("Error on writing from client.");
-        
+        // clean again to get from server
         bzero(buffer, 255);
-
+        //read
         n = read(sockfd, buffer, 255);
         if (n < 0)
             error("Error on reading from server");
 
         printf("Server: %s", buffer);
-
+        // if bye exit
         int i = strncmp("Bye", buffer, 3);
         if (i==0)
             break;
--- server.c
+++ server.c
@@ -1,78 +1,105 @@
-// Server port number must be greater than 1024 as ports 1-1024 are reserved ports for the system.
-// 65536 ports in total.
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+#include <unistd.h> 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
 void error(const char *msg) 
 {
-    perror(msg); // Interprets error number and outputs error desc using stderr
-    exit(1); // We require our program to terminate when we write the error function
+    perror(msg); 
+    exit(1); 
 }
+// argv[0] is the file name and argv[1] is portno
 
 int main(int argc, char *argv[]) 
-// These argc and argv[] refer to the command line arguments giver when the file is run in the terminal.
-// argc - total no of parameters including the filename (here, 2) 
-// argv[] is the array formed from the command line arguments. argv[0] = filename 
-// Refer : https://www.geeksforgeeks.org/command-line-arguments-in-c-cpp/
 {
     if (argc < 2)
     {
         fprintf(stderr, "Port number not provided. Program terminated.\n");
-        exit(1); // Represents EXIT_FAILURE or abnormal exit while exit(0) represents EXIT_SUCCESS or successful termination.
+        exit(1); 
     }
+    // SOCKFD IS FILE descriptor
+    // NEWSOCKFD IS NEW FILE descriptor, required after connection
+    // portno is the port number
+    // n is function sucessfull or not
+    // buffer[can be any number] first store in buffer and then pass in datastream
+    
 
     int sockfd, newsockfd, portno, n;
     char buffer[255];
 
-    struct sockaddr_in serv_addr, cli_addr;  // sockaddr_in gives us internet address <netinet.h>
-    socklen_t clilen; //socklen is a data type in <socket.h> 32 bit
+    //  {sockaddr_in gives internet address}
+    // chilen is length of sock  {socklen_t is datatype of 32 bit }
+
+    struct sockaddr_in serv_addr, cli_addr;  
+    socklen_t clilen; 
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    //AF_INET IS FOR IPV4 sockets
+    // SOCK_STREAM IS FOR TCP 
+    // 0 IS FOR TCP 
 
-    if (sockfd < 0) // i.e., socket() function resulted in failure.
+
+    
+    if (sockfd < 0) 
         error("Error opening socket");
 
-    bzero((char *) &serv_addr, sizeof(serv_addr)); // bzero() clears any data or text given inside as parameters.
     
-    portno = atoi(argv[1]); // argv[1] contains the port no specified with command line arguments.
+    
+    //bzero erases the data 
+    bzero((char *) &serv_addr, sizeof(serv_addr)); 
+    
+    //atoi convert string into integer
+
+    portno = atoi(argv[1]); 
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno); // htons() - host to network short also, see host to network long
+    serv_addr.sin_port = htons(portno); 
+
+    //** bind
+    // sockfd is type caster to sockaddr
 
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-        error("Binding Failed"); // If function fails, bind will return -1.
+        error("Binding Failed");
 
-    listen(sockfd, 5); // 5 is the max no of clients that can connect to the server.
+    // ** listen
+    // 5 stands for max number of connections can be any number
+    listen(sockfd, 5); 
     clilen = sizeof(cli_addr);
+
+
+    // ** accept connection
+
 
     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 
-    if (newsockfd < 0) // accept() fails
+    if (newsockfd < 0) 
         error("Error on accept");
 
-    while(1) // Code for communication between client and server.
-    {
-        bzero(buffer, 255); //  Clear the buffer. We have defined the buffer size as 255.
+    while(1) 
+    {   // clear buffer
+        bzero(buffer, 255);
+        //  read
         n = read(newsockfd, buffer, 255);
-        if (n < 0) // read() fails
+        if (n < 0) 
             error("Error on reading.");
         printf("Client: %s\n", buffer);
-
-        bzero(buffer, 255); // Clear the buffer so that we can use it to transmit the server's message.
-        fgets(buffer, 255, stdin); // <stdio.h> function reads bytes from stream.
+        // clear buffer
+        bzero(buffer, 255); 
+        // fget will get replay from server
+        fgets(buffer, 255, stdin); 
         n = write(newsockfd, buffer, strlen(buffer));
-        // fgets() reads from server, and write function sends it over the network.
+       
         if (n < 0) {
             error("Error on writing");
         }
-        int i = strncmp("Bye", buffer, 3); // Inbuilt function for string comparison
+        // strncmp inbuild function to compare string
+        int i = strncmp("Bye", buffer, 3); 
         if (i==0)
             break;
     }
